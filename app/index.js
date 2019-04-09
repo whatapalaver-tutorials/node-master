57da07513507d2eafb3746b74333c63272506acc
whatapalaver <warriorwomenblog@gmail.com>
2019-04-09 10:37:23 +0100

    Adding request path handling

--- app/index.js
+++ app/index.js
@@ -5,10 +5,23 @@
 
 // Dependencies
 const http = require('http');
+const url = require('url');
 
 // The server should respond to all requests with a string
 const server = http.createServer(function(req,res){
+  // Get the URL and parse it
+  let parsedUrl = url.parse(req.url, true);
+  
+  // Get path from URL
+  let path = parsedUrl.pathname;
+  let trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+  // Send the response
   res.end('Hello World\n');
+
+  // Log the path requested
+  console.log("Request received on path:", trimmedPath)
+  
 })
 
 // Start the server and have it listen on port 3000
